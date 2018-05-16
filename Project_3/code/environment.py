f824f9ab8c1038659d0601706f2b5a1ec1c1e777
Unknown <linzuolee@gmail.com>
2018-05-15 23:36:47 -0700

    pushed

--- Project_3/code/environment.py
+++ Project_3/code/environment.py
@@ -1,12 +1,10 @@
-import numpy as np
-
 class Environment:
-    def __init__(self,w, size):
+    def __init__(self, w, size):
         self.size = size
         self.w = w
         self.actions = {'up': 0, 'down': 1, 'left': 2, 'right': 3}
 
-    def is_out(self,x,y):
+    def is_out(self, x, y):
         if x < 0 or x >= self.size:
             return True
         if y < 0 or y >= self.size:
@@ -24,26 +22,22 @@ class Environment:
         if action == 'right':
             return 0,1
 
-    def get_next_state_prob(self, x,y, action):
+    def get_next_state_prob(self, x, y, action):
         omega = float(self.w/4)
-        states = [omega,omega,omega,omega,0]
+        states = [omega,omegagiit omega,omega,0]
         states[self.actions[action]] += 1 - self.w
-
+        res = {}
         for a in self.actions:
             dx, dy = self.get_diretion(a)
             if self.is_out(x+dx, y+dy):
                 states[4] += states[self.actions[a]]
                 states[self.actions[a]] = 0
-        return states
 
-
-
-env = Environment(0.4, 10)
-print(env.get_next_state_prob(0,0,'right'))
-        
-
-
-
-
-
+        for a in self.actions:
+            dx, dy = self.get_diretion(a)
+            key = (x+dx) + (y+dy)*10
+            value = states[self.actions[a]]
+            res[key] = value
+        res[x+y*10] = states[4]
+        return res
 
