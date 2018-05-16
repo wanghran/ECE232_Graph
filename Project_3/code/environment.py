e7ece22260fc6f687bb02ba832d9e07c69aadf68
Linzuo Li <linzuolee@gmail.com>
2018-05-15 23:08:24 -0700

    bug fixes on direction

--- Project_3/code/environment.py
+++ Project_3/code/environment.py
@@ -4,19 +4,46 @@ class Environment:
     def __init__(self,w, size):
         self.size = size
         self.w = w
-        self.actions = [{1,0},{0,1}, {-1,0}, {0,-1}]
+        self.actions = {'up': 0, 'down': 1, 'left': 2, 'right': 3}
 
-    def get_actions(self,x,y):
-        res = []
-        for dx, dy in self.actions:
-            if not self.is_out(x+dx, y+dy):
-                res.append({dx, dy})
-    
     def is_out(self,x,y):
         if x < 0 or x >= self.size:
             return True
         if y < 0 or y >= self.size:
             return True
         return False
+
+
+    def get_diretion(self, action):
+        if action == 'up':
+            return 0,-1
+        if action == 'down':
+            return 0,1
+        if action == 'left':
+            return -1,0
+        if action == 'right':
+            return 0,1
+
+    def get_next_state_prob(self, x,y, action):
+        omega = float(self.w/4)
+        states = [omega,omega,omega,omega,0]
+        states[self.actions[action]] += 1 - self.w
+
+        for a in self.actions:
+            dx, dy = self.get_diretion(a)
+            if self.is_out(x+dx, y+dy):
+                states[4] += states[self.actions[a]]
+                states[self.actions[a]] = 0
+        return states
+
+
+
+env = Environment(0.4, 10)
+print(env.get_next_state_prob(0,0,'right'))
+        
+
+
+
+
 
 
