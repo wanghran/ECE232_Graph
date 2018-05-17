4230ff4b48bd93fff3690e430e24eeaaccdd63fb
Liang Qiu <liangqiu@g.ucla.edu>
2018-05-16 21:07:40 -0700

    q2

--- Project_3/code/value_iteration.py
+++ Project_3/code/value_iteration.py
@@ -1,15 +1,17 @@
+import numpy as np
 import RF1
 import RF2
 import environment
-import numpy as np
 import heat_map
+import value_map
 
 EPSILON = 0.01
 
 
 def value_iteration(env, reward, S, A, gamma):
     V = np.zeros(len(S))
-    delta = 1000  # large number
+    V_tmp = np.zeros(len(S))
+    delta = 10000  # large number
 
     while delta > EPSILON:
         delta = 0
@@ -17,15 +19,19 @@ def value_iteration(env, reward, S, A, gamma):
             v = V[s]
             # find the maximum action-value
             maximum = 0
-            for a in ['up', 'down', 'left', 'right']:
+            for a in A:
                 P = env.get_next_state_prob(s % 10, s / 10, a)
                 summation = 0
                 for s_prime, possibility in P.items():
-                    summation = summation + possibility * (reward[s % 10, s / 10] + gamma * V[s_prime])
+                    summation += possibility * (reward[s % 10, s / 10] + gamma * V[s_prime])
                 if summation > maximum:
                     maximum = summation
-            V[s] = maximum
-            delta = max(delta, abs(v-V[s]))
+                    a_optimal = a
+            V_tmp[s] = maximum
+            delta = max(delta, abs(v - V_tmp[s]))
+        for s in S:
+            V[s] = V_tmp[s]
+
     # for s in S:
     #     pi[s] = find the max index
 
@@ -33,22 +39,25 @@ def value_iteration(env, reward, S, A, gamma):
 
 
 def main():
-    w = 0.1
     size = 10
-    gamma = 0.8
-    env = environment.Environment(w, size)
-    reward_1 = RF2.RF2()
-    reward = reward_1.reward
-    S = range(size * size)
     A = ['up', 'down', 'left', 'right']
+    w = 0.1
+    gamma = 0.8
+    reward = RF2.RF2().reward
 
-    V = value_iteration(env, reward, S, A, gamma)
+    env = environment.Environment(w, size)
+    S = range(size * size)
 
-    # draw heat map
-    V_tmp = np.zeros((size, size))
+    V_optimal = value_iteration(env, reward, S, A, gamma)
+
+    V_mesh = np.zeros((size, size))
     for s in S:
-        V_tmp[s % 10, s / 10] = V[s]
-    heat_map.heat_map(V_tmp, 'Heat Map of State Value')
+        V_mesh[s % 10, s / 10] = V_optimal[s]
+
+    # draw value_map
+    value_map.value_map(V_mesh, 'Q2')
+    # draw heat_map
+    heat_map.heat_map(V_mesh, 'Q3')
 
 
 if __name__ == '__main__':
