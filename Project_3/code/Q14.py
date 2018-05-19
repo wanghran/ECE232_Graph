22a4e3bdc1a65f34b184081138b5f1208402cf5f
Haoran Wang <wanghran@gmail.com>
2018-05-18 22:41:37 -0700

    14 revision

--- Project_3/code/Q14.py
+++ Project_3/code/Q14.py
@@ -1,12 +1,16 @@
 from environment import Environment
+from RF1 import RF1
 import value_iteration
 import value_map
+from irl import irl
 
+og_reward = RF1().reward
 env = Environment(0.1, 10)
-opt_reward = None
 S = range(size * size)
 A = ['up', 'down', 'left', 'right']
 gamma = 0.8
+ogV, ogP = value_iteration(env, og_reward, S, A, gamma)
+opt_reward = irl(100, 4, None, ogP, gamma, og_reward)
 optV, _ = value_iteration(env, opt_reward, S, A, gamma)
 V_mesh = np.zeros((size, size))
 for s in S:
