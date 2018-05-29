fe0bf2eb3421cd54f0c25e76e4446bd18914e89a
Unknown <linzuolee@gmail.com>
2018-05-28 22:56:14 -0700

    Added clean script

---
+++ Project_4/src/clean_data.py
@@ -0,0 +1,32 @@
+import re
+import csv
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("input", help="input file name",
+                    type=str)
+parser.add_argument("output", help="output file name",
+                    type=str)
+args = parser.parse_args()
+
+
+with open('./' + args.input, 'r', encoding = "ISO-8859-1") as f:
+    data = f.readlines()
+p = re.compile(r'\((\d{4}|\?\?\?\?)(\)|/[a-zA-Z]*\))')
+
+with open('./'+args.output, "w+") as f:
+    for line in data:
+        temp = line.split('\t\t')
+        if len(temp) <= 1:
+            continue
+        actor = temp[0]
+        movies = []
+        for i in range (1, len(temp)):
+            movie = temp[i]
+            if p.search(movie) != None :
+                movies.append(movie[:p.search(movie).end()])
+            movies = list(set(movies))
+        if len(movies) < 10:
+            continue
+        str_movies = '\t'.join(movies)
+        f.write(actor + ' || ')
+        f.write(str_movies + "\n")
