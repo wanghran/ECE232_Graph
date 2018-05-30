ff565041893cc5eac9bd052e3b92d0a3a98dce2b
Unknown <linzuolee@gmail.com>
2018-05-29 20:49:47 -0700

    append file

--- Project_4/src/clean_data.py
+++ Project_4/src/clean_data.py
@@ -9,7 +9,7 @@ parser.add_argument("output", help="output file name",
 args = parser.parse_args()
 
 
-with open( args.input, 'r', encoding = "ISO-8859-1") as f:
+with open( args.input + 'actor_movies.txt', 'r', encoding = "ISO-8859-1") as f:
     data = f.readlines()
 p = re.compile(r'\((\d{4}|\?\?\?\?)(\)|/[a-zA-Z]*\))')
 
@@ -27,6 +27,28 @@ with open( args.output, "w+") as f:
             movies = list(set(movies))
         if len(movies) < 10:
             continue
-        str_movies = '\t'.join(movies)
-        f.write(actor + ' || ')
+        str_movies = '\t\t'.join(movies)
+        f.write(actor + '||')
+        f.write(str_movies + "\n")
+
+with open( args.input + 'actress_movies.txt', 'r', encoding = "ISO-8859-1") as f:
+    data = f.readlines()
+p = re.compile(r'\((\d{4}|\?\?\?\?)(\)|/[a-zA-Z]*\))')
+
+with open( args.output, "a") as f:
+    for line in data:
+        temp = line.split('\t\t')
+        if len(temp) <= 1:
+            continue
+        actor = temp[0]
+        movies = []
+        for i in range (1, len(temp)):
+            movie = temp[i]
+            if p.search(movie) != None :
+                movies.append(movie[:p.search(movie).end()])
+            movies = list(set(movies))
+        if len(movies) < 10:
+            continue
+        str_movies = '\t\t'.join(movies)
+        f.write(actor + '||')
         f.write(str_movies + "\n")
