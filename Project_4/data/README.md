b0d5b44820fd40641a2704bd10d255825ce8a4dd
Unknown <linzuolee@gmail.com>
2018-05-29 23:22:38 -0700

    Added generate.sh

---
+++ Project_4/data/README.md
@@ -0,0 +1,1 @@
+Download file from [here](https://ucla.app.box.com/s/z45q3g5zrpay8b8gtbql6ojaecb7kj2u)
---
+++ Project_4/src/make_edgelist.sh
@@ -0,0 +1,2 @@
+python clean_data.py ../data/ ../output/out.txt
+python create_edgelist.py ../output/out.txt
