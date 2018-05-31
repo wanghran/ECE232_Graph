c3dee350f02e25e35218a79012f7bdcdf6dab81e
Haoran Wang <wanghran@gmail.com>
2018-05-30 22:22:17 -0700

    movie_dict_v2 produces txt file

--- Project_4/src/movie_dict_v2.py
+++ Project_4/src/movie_dict_v2.py
@@ -2,28 +2,36 @@ import argparse
 
 paser = argparse.ArgumentParser()
 paser.add_argument('input', help='input file', type=str)
+paser.add_argument('output', help='output file', type=str)
 args = paser.parse_args()
 
+data = None
+with open(args.input, 'r', encoding='ISO-8859-1') as f:
+    data = f.readlines()
 
-def MD(input=args.input):
-    data = None
-    with open(args.input, 'r', encoding='ISO-8859-1') as f:
-        data = f.readlines()
+movie_dict = {}
+for line in data:
+    temp = line.split('||')
+    ident = int(temp[0])
+    movies = temp[2].split('\t\t')
+    for movie in movies:
+        if(movie in movie_dict):
+            assert type(movie_dict[movie]) is list
+            movie_dict[movie].extend([ident])
+        else:
+            movie_dict[movie] = [ident]
 
-    movie_dict = {}
-    for line in data:
-        temp = line.split('||')
-        ident = int(temp[0])
-        movies = temp[2].split('\t\t')
-        for movie in movies:
-            if(movie in movie_dict):
-                assert type(movie_dict[movie]) is list
-                movie_dict[movie].extend([ident])
-            else:
-                movie_dict[movie] = [ident]
+for movie in list(movie_dict):
+    if(len(movie_dict[movie]) < 5):
+        del(movie_dict[movie])
 
-    for movie in list(movie_dict):
-        if(len(movie_dict[movie]) < 5):
-            del(movie_dict[movie])
-
-    return movie_dict
+movie_id = 0
+with open(args.output, 'w+') as f:
+    for movie in movie_dict:
+        f.write('%d||' % movie_id)
+        movie_d = movie.replace('\n', '')
+        f.write(movie_d + "||")
+        for i in range(len(movie_dict[movie]) - 1):
+            f.write(str(movie_dict[movie][i]) + '\t\t')
+        f.write(str(movie_dict[movie][-1]) + '\n')
+        movie_id += 1
