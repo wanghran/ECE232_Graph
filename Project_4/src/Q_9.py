464b0f641423070af9e022fea88c79535048dd1d
Liang Qiu <liangqiu@g.ucla.edu>
2018-06-01 23:31:47 -0700

    9 10 11

---
+++ Project_4/src/Q_9.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+with open('../data/movie_rating.txt', 'r', encoding='ISO-8859-1') as f:
+    movie_rating = f.readlines()
+    rates = {}
+    for line in movie_rating:
+        temp = line.split('\t\t')
+        rates[temp[0]] = float(temp[1].split('\n')[0])
+
+with open('../output/movie_dict.txt', 'r') as f:
+    movie_dict = f.readlines()
+    movies = []
+    for line in movie_dict:
+        temp = line.split('||')
+        movies.append(temp[1])
+
+with open('../output/movie_edge_list.txt', 'r') as f:
+    movie_edge_data = f.readlines()
+
+    for movie in [10062, 26989, 85671]:
+        neighs = []
+        rates_neigh = []
+        for line in movie_edge_data:
+            temp = line.split('\t')
+            if temp[0] == '\n':
+                continue
+            if int(temp[0]) == movie:
+                neigh = movies[int(temp[1])]
+                neighs.append(neigh)
+                if neigh in rates:
+                    rates_neigh.append(rates[neigh])
+            if int(temp[1]) == movie:
+                neigh = movies[int(temp[0])]
+                neighs.append(neigh)
+                if neigh in rates:
+                    rates_neigh.append(rates[neigh])
+        # the histogram of the data
+        n, bins, patches = plt.hist(rates_neigh)
+        plt.show()
+        print('Average rating of neighbors:', np.average(rates_neigh))
